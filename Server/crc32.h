474b09324e44727d39bbb34b5e8d83386f813db1
Unknown <viperman1271@hotmail.com>
2019-09-07 15:18:54 -0400

    crc32 hashing header

---
+++ Server/crc32.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <cstdint>
+#include <stdint.h>
+
+namespace detail
+{
+    // FNV-1a 32bit hashing algorithm.
+    constexpr std::uint32_t fnv1a_32(char const* s, std::size_t count)
+    {
+        return ((count ? fnv1a_32(s, count - 1) : 2166136261u) ^ s[count]) * 16777619u;
+    }
+
+    constexpr std::uint32_t fnv1a_32(wchar_t const* s, std::size_t count)
+    {
+        return ((count ? fnv1a_32(s, count - 1) : 2166136261u) ^ s[count]) * 16777619u;
+    }
+}    // namespace detail
+
+constexpr std::uint32_t hash(char const* s, std::size_t count)
+{
+    return detail::fnv1a_32(s, count);
+}
+
+constexpr std::uint32_t hash(wchar_t const* s, std::size_t count)
+{
+    return detail::fnv1a_32(s, count);
+}
+
+// FNV1a c++11 constexpr compile time hash functions, 32 and 64 bit
+// str should be a null terminated string literal, value should be left out 
+// e.g hash_32_fnv1a_const("example")
+// code license: public domain or equivalent
+// post: https://notes.underscorediscovery.com/constexpr-fnv1a/
+
+constexpr uint32_t val_32_const = 0x811c9dc5;
+constexpr uint32_t prime_32_const = 0x1000193;
+constexpr uint64_t val_64_const = 0xcbf29ce484222325;
+constexpr uint64_t prime_64_const = 0x100000001b3;
+
+inline constexpr uint32_t hash_32_fnv1a_const(const char* const str, const uint32_t value = val_32_const) noexcept
+{
+    return (str[0] == '\0') ? value : hash_32_fnv1a_const(&str[1], (value ^ uint32_t(str[0])) * prime_32_const);
+}
+
+inline constexpr uint32_t hash_32_fnv1a_const(const wchar_t* const str, const uint32_t value = val_32_const) noexcept
+{
+    return (str[0] == '\0') ? value : hash_32_fnv1a_const(&str[1], (value ^ uint32_t(str[0])) * prime_32_const);
+}
+
+inline constexpr uint64_t hash_64_fnv1a_const(const char* const str, const uint64_t value = val_64_const) noexcept
+{
+    return (str[0] == '\0') ? value : hash_64_fnv1a_const(&str[1], (value ^ uint64_t(str[0])) * prime_64_const);
+}
+
+inline constexpr uint64_t hash_64_fnv1a_const(const wchar_t* const str, const uint64_t value = val_64_const) noexcept
+{
+    return (str[0] == '\0') ? value : hash_64_fnv1a_const(&str[1], (value ^ uint64_t(str[0])) * prime_64_const);
+}
+
+inline constexpr uint32_t hash_general()
+{
+
+}
