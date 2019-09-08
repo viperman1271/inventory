6403e54b70b712a83ee625a945fad12f9a73cfeb
Unknown <viperman1271@hotmail.com>
2019-09-08 16:42:03 -0400

    Adding users via user handler supported

--- snapd/handlers/userhandler.cpp
+++ snapd/handlers/userhandler.cpp
@@ -1,6 +1,10 @@
 #include "stdafx.h"
 #include <handlers/userhandler.h>
 
+#include <database.h>
+
+extern std::unique_ptr<database> g_database;
+
 void user_handler::handleGet(web::http::http_request message)
 {
     std::wstring response = user().getAll();
@@ -11,6 +15,14 @@ void user_handler::handlePut(web::http::http_request message)
 {
     if (message.request_uri().path() == U("/user/create"))
     {
+        std::wstring body = message.extract_string().get();
+        
+        user user;
+        user.setAll(body);
 
+        if (user.isValid() && !g_database->userExists(user))
+        {
+            g_database->addUser(user);
+        }
     }
 }
