4e79c055ad1c3c7d1afc2aa25bd247b571f4d941
Unknown <viperman1271@hotmail.com>
2019-09-08 16:40:22 -0400

    Adding some overrides for setAll to handle different parameters

--- libsnap/include/libsnap/object.h
+++ libsnap/include/libsnap/object.h
@@ -7,7 +7,16 @@ class object
 {
 public:
     virtual const std::wstring getAll() const = 0;
+    inline void setAll(const char* data) { handleSetAll(data); }
+    inline void setAll(const std::string& data) { setAll(data.c_str()); }
+    inline void setAll(const std::wstring& data) 
+    {
+        std::unique_ptr<char> pszData(new char[data.size() + 1]);
+        wcstombs(pszData.get(), data.c_str(), data.size() + 1);
+        setAll(pszData.get());
+    }
 
 protected:
     const std::wstring getJsonStr(json_object* jobj) const;
+    virtual void handleSetAll(const char* data) {}
 };
