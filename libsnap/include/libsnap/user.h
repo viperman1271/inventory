466ba733ba990bf56bcd37101dabf7e4c50be23c
Unknown <viperman1271@hotmail.com>
2019-09-08 16:42:28 -0400

    Added a bunch of accessor and mutators to user

--- libsnap/include/libsnap/user.h
+++ libsnap/include/libsnap/user.h
@@ -7,11 +7,38 @@
 class user : public object
 {
 public:
+    user();
+
     virtual const std::wstring getAll() const override;
+    virtual void handleSetAll(const char* data) override;
+
     json_object* getJsonObject() const;
+
+    const unsigned int getUserId() const { return m_UserId; }
+    const bool getIsPasswordHashed() const { return m_PasswordHashed; }
+    const std::string& getUsername() const { return m_Username; }
+    const std::string& getEmail() const { return m_Email; }
+    const std::string& getDatabase() const { return m_Database; }
+    const std::string& getPassword() const { return m_Password; }
+
+    void setUserId(const unsigned int userId) { m_UserId = userId; }
+    void setUsername(const std::string& username) { m_Username = username; }
+    void setEmail(const std::string& email) { m_Email = email; }
+    void setPassword(const std::string& password) { m_Password = password; m_PasswordHashed = false; }
+    void setPasswordHash(const std::string& password) { m_Password = password; m_PasswordHashed = true; }
+
+    bool isValid() const { return !m_Username.empty() && !m_Email.empty() && !m_Password.empty(); }
+
+    inline bool operator==(const user& rhs) const
+    {
+        return isValid() == rhs.isValid() && m_Username == rhs.m_Username && m_Email == rhs.m_Email;
+    }
 
 private:
     unsigned int m_UserId;
+    bool m_PasswordHashed;
+    std::string m_Username;
     std::string m_Email;
     std::string m_Database;
+    std::string m_Password;
 };
--- libsnap/objectmodel/user.cpp
+++ libsnap/objectmodel/user.cpp
@@ -1,9 +1,45 @@
 #include <libsnap/user.h>
 
+user::user()
+    : m_UserId(-1)
+    , m_PasswordHashed(false)
+{
+
+}
+
 const std::wstring user::getAll() const
 {
     json_object* jobj = getJsonObject();
     return getJsonStr(jobj);
+}
+
+void user::handleSetAll(const char* data)
+{
+    json_object* jobj = json_tokener_parse(data);
+    json_object_object_foreach(jobj, key, val)
+    {
+        json_object* jobjval = json_object_object_get(jobj, key);
+        if (strcmp(key, "UserId") == 0)
+        {
+            m_UserId = json_object_get_int(jobjval);
+        }
+        else if (strcmp(key, "Username") == 0)
+        {
+            m_Username = json_object_get_string(jobjval);
+        }
+        else if (strcmp(key, "Password") == 0)
+        {
+            m_Password = json_object_get_string(jobjval);
+        }
+        else if (strcmp(key, "IsHashed") == 0)
+        {
+            m_PasswordHashed = json_object_get_boolean(jobjval);
+        }
+        else if (strcmp(key, "Email") == 0)
+        {
+            m_Email = json_object_get_string(jobjval);
+        }
+    }
 }
 
 json_object* user::getJsonObject() const
@@ -11,6 +47,9 @@ json_object* user::getJsonObject() const
     json_object* jobj = json_object_new_object();
     {
         json_object_object_add(jobj, "UserId", json_object_new_int(m_UserId));
+        json_object_object_add(jobj, "Username", json_object_new_string(m_Username.c_str()));
+        json_object_object_add(jobj, "Password", json_object_new_string(m_Password.c_str()));
+        json_object_object_add(jobj, "IsHashed", json_object_new_boolean(m_PasswordHashed));
         json_object_object_add(jobj, "Email", json_object_new_string(m_Email.c_str()));
     }
     return jobj;
