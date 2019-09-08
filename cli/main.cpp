da730a8b945f850c5bca2d59e530a607001934cd
Unknown <viperman1271@hotmail.com>
2019-09-08 18:33:13 -0400

    Added support to login for cli

--- cli/main.cpp
+++ cli/main.cpp
@@ -20,6 +20,10 @@ int main(int argc, char** argv)
     createCommand->add_option("--email", email, "create a new user with the given name");
     createCommand->add_option("--password", password, "create a new user with the given name");
 
+    CLI::App* loginCommand = userCommand->add_subcommand("login", "logs into the server using provided password");
+    loginCommand->add_option("--name", username, "username to use for login");
+    loginCommand->add_option("--password", password, "password for the user");
+
     try 
     {
         app.parse(argc, argv);
@@ -30,30 +34,60 @@ int main(int argc, char** argv)
     }
 
     //Create new user
-    if (username.size() > 0 && email.size() > 0 && password.size() > 0)
+    if (createCommand->parsed())
     {
-        user user;
-        user.setUsername(username);
-        user.setEmail(email);
-        user.setPassword(password);
+        if(username.size() > 0 && email.size() > 0 && password.size() > 0)
+        {
+            user user;
+            user.setUsername(username);
+            user.setEmail(email);
+            user.setPassword(password);
 
-        json_object* jobj = user.getJsonObject();
-        const char* jsonString = json_object_to_json_string(jobj);
+            json_object* jobj = user.getJsonObject();
+            const char* jsonString = json_object_to_json_string(jobj);
 
-        concurrency::streams::container_buffer<std::vector<uint8_t>> buf;
+            concurrency::streams::container_buffer<std::vector<uint8_t>> buf;
 
-        web::uri_builder uri(U("http://localhost"));
-        std::wstring addr = uri.to_uri().to_string();
-        web::http::client::http_client client(addr);
+            web::uri_builder uri(U("http://localhost"));
+            std::wstring addr = uri.to_uri().to_string();
+            web::http::client::http_client client(addr);
 
-        web::http::http_request msg(web::http::methods::PUT);
-        msg.set_body(jsonString);
-        msg.set_response_stream(buf.create_ostream());
+            web::http::http_request msg(web::http::methods::PUT);
+            msg.set_body(jsonString);
+            msg.set_response_stream(buf.create_ostream());
 
-        web::uri_builder builder(U("/user/create"));
-        msg.set_request_uri(builder.to_uri());
+            web::uri_builder builder(U("/user/create"));
+            msg.set_request_uri(builder.to_uri());
 
-        auto resp = client.request(msg).get();
+            auto resp = client.request(msg).get();
+        }
+    }
+    else if (loginCommand->parsed())
+    {
+        if (username.size() > 0 && password.size() > 0)
+        {
+            user user;
+            user.setUsername(username);
+            user.setPassword(password);
+            
+            json_object* jobj = user.getJsonObject();
+            const char* jsonString = json_object_to_json_string(jobj);
+
+            concurrency::streams::container_buffer<std::vector<uint8_t>> buf;
+
+            web::uri_builder uri(U("http://localhost"));
+            std::wstring addr = uri.to_uri().to_string();
+            web::http::client::http_client client(addr);
+
+            web::http::http_request msg(web::http::methods::PUT);
+            msg.set_body(jsonString);
+            msg.set_response_stream(buf.create_ostream());
+
+            web::uri_builder builder(U("/login"));
+            msg.set_request_uri(builder.to_uri());
+
+            auto resp = client.request(msg).get();
+        }
     }
 
     return 0;
