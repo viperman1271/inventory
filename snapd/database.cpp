3c2b261d32bb25889df846d0f323f5ae05786f6f
Unknown <viperman1271@hotmail.com>
2019-09-08 18:33:35 -0400

    Database class can now encode/decode passwords and loads users correctly

--- snapd/database.cpp
+++ snapd/database.cpp
@@ -6,12 +6,23 @@
 
 #include <libsnap/user.h>
 
+#include <cryptopp/config.h>
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/pwdbased.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/osrng.h>
+
+const unsigned int iterations = 1000;
+
 database::database()
 {
     if (!file::utils::fileExists("users.json"))
     {
         writeUsers();
     }
+
+    loadUsers();
 }
 
 database::~database()
@@ -19,23 +30,34 @@ database::~database()
 
 }
 
-bool database::userExists(const user& in_User) const
+bool database::userExists(const user& in_user) const
 {
-    auto_lock<std::recursive_mutex> lock(m_Mutex);
-
-    for (const user& _user : m_Users)
-    {
-        if (_user == in_User)
-        {
-            return true;
-        }
-    }
-
-    return false;
+    return getStoredUser(in_user) != nullptr;
 }
 
-void database::addUser(const user& in_user)
+bool database::addUser(const user& in_user)
 {
+    //////////////////////////////////////////////////////////////////////////
+
+
+    std::string encoded = "FFEEDDCCBBAA99887766554433221100";
+    std::string decoded;
+
+    CryptoPP::HexDecoder decoder;
+    decoder.Put((CryptoPP::byte*)encoded.data(), encoded.size());
+    decoder.MessageEnd();
+
+    CryptoPP::word64 size = decoder.MaxRetrievable();
+    if (size && size <= SIZE_MAX)
+    {
+        decoded.resize(size);
+        decoder.Get((CryptoPP::byte*)&decoded[0], decoded.size());
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+
+
+
     auto_lock<std::recursive_mutex> lock(m_Mutex);
 
     user _user(in_user);
@@ -49,10 +71,68 @@ void database::addUser(const user& in_user)
         const unsigned int lastUserId = m_Users.back().getUserId();
         _user.setUserId(lastUserId + 1);
     }
-    
+
+    CryptoPP::AutoSeededRandomPool rng;
+
+    CryptoPP::SecByteBlock pwsalt(CryptoPP::SHA256::DIGESTSIZE);
+    rng.GenerateBlock(pwsalt, pwsalt.size());
+
+    CryptoPP::SecByteBlock derivedkey(CryptoPP::SHA256::DIGESTSIZE);
+
+    CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA256> pbkdf;
+    pbkdf.DeriveKey(derivedkey
+        , derivedkey.size()
+        , 0x00
+        , reinterpret_cast<const CryptoPP::byte*>(in_user.getPassword().c_str())
+        , in_user.getPassword().length()
+        , pwsalt
+        , pwsalt.size()
+        , iterations
+    );
+
+    CryptoPP::HexEncoder encoder;
+
+    std::string hexEncodedPassword;
+    encoder.Attach(new CryptoPP::StringSink(hexEncodedPassword));
+    encoder.Put(derivedkey, derivedkey.size());
+    encoder.MessageEnd();
+
+    std::string hexEncodedSalt;
+    encoder.Attach(new CryptoPP::StringSink(hexEncodedSalt));
+    encoder.Put(pwsalt, pwsalt.size());
+    encoder.MessageEnd();
+
+    _user.setPasswordHash(hexEncodedPassword);
+    _user.setSalt(hexEncodedSalt);
+
     m_Users.push_back(_user);
 
     writeUsers();
+
+    return true;
+}
+
+bool database::validatePassword(const user& in_user) const
+{
+    if (!userExists(in_user))
+    {
+        return false;
+    }
+
+    const user* storedUser = getStoredUser(in_user);
+    if (storedUser == nullptr)
+    {
+        return false;
+    }
+
+    const std::string& encoded = storedUser->getSalt();
+    std::string decoded = decodeString(encoded);
+
+    CryptoPP::SecByteBlock passwordSalt(reinterpret_cast<const CryptoPP::byte*>(decoded.c_str()), decoded.size());
+
+    std::string encodedPassword = hashAndEncodePassword(in_user.getPassword(), passwordSalt);
+
+    return encodedPassword == storedUser->getPassword();
 }
 
 void database::writeUsers()
@@ -74,4 +154,126 @@ void database::writeUsers()
     m_UsersFile = std::ofstream("users.json");
     m_UsersFile.write(jsonString, strlen(jsonString));
     m_UsersFile.close();
+}
+
+void database::loadUsers()
+{
+    std::ifstream usersFile = std::ifstream("users.json");
+    usersFile.seekg(0, std::ios::end);
+    const size_t size = usersFile.tellg();
+
+    usersFile.seekg(0, std::ios::beg);
+
+    std::unique_ptr<char> fileBuffer(new char[size + 1]);
+    memset(fileBuffer.get(), 0, size + 1);
+    usersFile.read(reinterpret_cast<char*>(fileBuffer.get()), size);
+
+
+    json_object* jobj = json_tokener_parse(fileBuffer.get());
+    json_object_object_foreach(jobj, key, val)
+    {
+        json_object* jarray = json_object_object_get(jobj, key);
+        json_type type = json_object_get_type(val);
+
+        assert(type == json_type_array);
+        const size_t arraylen = json_object_array_length(jarray);
+
+        for (unsigned int i = 0; i < arraylen; ++i)
+        {
+            json_object* userValue = json_object_array_get_idx(jarray, i); /*Getting the array element at position i*/
+            const json_type type = json_object_get_type(userValue);
+
+            user _user;
+            json_object_object_foreach(userValue, key, val)
+            {
+                json_object* userObjValue = json_object_object_get(userValue, key);
+
+                if (strcmp(key, "UserId") == 0)
+                {
+                    _user.setUserId(json_object_get_int(userObjValue));
+                }
+                else if (strcmp(key, "Username") == 0)
+                {
+                    _user.setUsername(json_object_get_string(userObjValue));
+                }
+                else if (strcmp(key, "Password") == 0)
+                {
+                    _user.setPasswordHash(json_object_get_string(userObjValue));
+                }
+                else if (strcmp(key, "Email") == 0)
+                {
+                    _user.setEmail(json_object_get_string(userObjValue));
+                }
+                else if (strcmp(key, "Salt") == 0)
+                {
+                    _user.setSalt(json_object_get_string(userObjValue));
+                }
+            }
+
+            m_Users.push_back(_user);
+        }
+     }
+}
+
+std::string database::hashAndEncodePassword(const std::string& in_plainTextPassword, const CryptoPP::SecByteBlock& in_passwordSalt) const
+{
+    CryptoPP::SecByteBlock derivedkey(CryptoPP::SHA256::DIGESTSIZE);
+
+    CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA256> pbkdf;
+    pbkdf.DeriveKey(derivedkey
+        , derivedkey.size()
+        , 0x00
+        , reinterpret_cast<const CryptoPP::byte*>(in_plainTextPassword.c_str())
+        , in_plainTextPassword.length()
+        , in_passwordSalt
+        , in_passwordSalt.size()
+        , iterations
+    );
+
+    return encodeString(derivedkey);
+}
+
+std::string database::encodeString(const CryptoPP::SecByteBlock& in_textToEncode) const
+{
+    CryptoPP::HexEncoder encoder;
+
+    std::string hexEncodedPassword;
+    encoder.Attach(new CryptoPP::StringSink(hexEncodedPassword));
+    encoder.Put(in_textToEncode, in_textToEncode.size());
+    encoder.MessageEnd();
+
+    return hexEncodedPassword;
+}
+
+std::string database::decodeString(const std::string& in_encodedString) const
+{
+    std::string decoded;
+
+    CryptoPP::HexDecoder decoder;
+    decoder.Put((CryptoPP::byte*)in_encodedString.data(), in_encodedString.size());
+    decoder.MessageEnd();
+
+    CryptoPP::word64 size = decoder.MaxRetrievable();
+    if (size && size <= SIZE_MAX)
+    {
+        decoded.resize(size);
+        decoder.Get((CryptoPP::byte*)&decoded[0], decoded.size());
+    }
+
+    return decoded;
+}
+
+const user* database::getStoredUser(const user& in_user) const
+{
+    auto_lock<std::recursive_mutex> lock(m_Mutex);
+
+    for (const user& _user : m_Users)
+    {
+        if (_user == in_user)
+        {
+            return &_user;
+        }
+    }
+
+    return nullptr;
 }
--- snapd/database.h
+++ snapd/database.h
@@ -2,17 +2,27 @@
 
 #include <libsnap/user.h>
 
+#include <cryptopp/secblock.h>
+
 class database
 {
 public:
     database();
     ~database();
 
-    bool userExists(const user& user) const;
-    void addUser(const user& user);
+    bool userExists(const user& in_user) const;
+    bool addUser(const user& in_user);
+    bool validatePassword(const user& in_user) const;
 
 private:
     void writeUsers();
+    void loadUsers();
+
+    std::string hashAndEncodePassword(const std::string& in_plainTextPassword, const CryptoPP::SecByteBlock& in_passwordSalt) const;
+    std::string encodeString(const CryptoPP::SecByteBlock& in_passwordSalt) const;
+    std::string decodeString(const std::string& in_encodedString) const;
+
+    const user* getStoredUser(const user& in_user) const;
 
 private:
     mutable std::recursive_mutex m_Mutex;
